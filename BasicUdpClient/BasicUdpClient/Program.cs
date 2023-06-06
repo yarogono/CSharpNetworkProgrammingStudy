d09026c0de88988869e7b6e3af62f820c1036d38
naocya@naver.com <naocya@naver.com>
2023-06-06 12:20:00 +0900

    UdpClient 추가 및 UdpServer 코드 수정

---
+++ BasicUdpClient/BasicUdpClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicUdpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (1) UdpClient 객체 성성
+            UdpClient udpClient = new UdpClient();
+            string msg = "안녕하세요.";
+            byte[] datagram = Encoding.UTF8.GetBytes(msg);
+
+            // (2) 데이터 송신
+            udpClient.Send(datagram, datagram.Length, "127.0.0.1", 9999);
+            Console.WriteLine("[Send] 127.0.0.1:7777 로 {0} 바이트 전송", datagram.Length);
+
+            // (3) 데이타 수신
+            IPEndPoint epRemote = new IPEndPoint(IPAddress.Any, 0);
+            byte[] bytes = udpClient.Receive(ref epRemote);
+            Console.WriteLine("[Receive] {0} 로부터 {1} 바이트 수신", epRemote.ToString(), bytes.Length);
+
+            // (4) UdpClient 객체 닫기
+            udpClient.Close();
+        }
+    }
+}
---
+++ BasicUdpServer/BasicUDP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicUdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic UDP Server!");
+            UdpServer("127.0.0.1", 9999);
+        }
+
+        static void UdpServer(string server, int port)
+        { 
+            UdpClient udpServer = new UdpClient(port);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("수신 대기 . . .");
+
+                    IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    Byte[] receiveBytes = udpServer.Receive(ref remoteIpEndPoint);
+
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+
+                    // 수신 데이터와 보낸 곳 정보 표시
+                    Console.WriteLine("수신: {0}Bytes {1}", receiveBytes.Length, returnData);
+                    Console.WriteLine("보낸 곳 IP=" 
+                                            + remoteIpEndPoint.Address.ToString
+                                            + " 포트 번호= "
+                                            + remoteIpEndPoint.Port.ToString());
+
+                    // 수신 데이터를 대문자로 변환하여 보낸다.
+                    returnData = returnData.ToUpper();
+
+                    byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(returnData);
+
+
+                    // 보낸 곳에 답변
+                    Console.WriteLine("답변 데이터: {0}Bytes {1}", sendBytes.Length, returnData);
+
+                    udpServer.Send(sendBytes, sendBytes.Length, remoteIpEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            udpServer.Close();
+
+            Console.Read();
+        }
+    }
+}
