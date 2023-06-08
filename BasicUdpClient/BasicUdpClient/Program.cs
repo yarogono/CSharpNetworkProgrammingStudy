86c7d7912f1010f7601d152b4b67cff857a55a6e
naocya@naver.com <naocya@naver.com>
2023-06-09 03:11:16 +0900

    클라이언트가 입력한 문자로 전송되도록 변경

--- BasicUdpClient/BasicUdpClient/Program.cs
+++ BasicUdpClient/BasicUdpClient/Program.cs
@@ -8,10 +8,27 @@ namespace BasicUdpClient
     {
         static void Main(string[] args)
         {
+
+            while (true)
+            {
+                Console.WriteLine("UDP 서버로 전송할 문자를 입력하세요.");
+                Console.WriteLine("종료를 하기 위해서는 exit 을 입력해주세요.");
+                string input = Console.ReadLine();
+
+                if (input.Equals("exit")) return;
+
+                UdpSendToServer(input);
+            }
+
+
+
+        }
+
+        private static void UdpSendToServer(string input)
+        {
             // (1) UdpClient 객체 성성
             UdpClient udpClient = new UdpClient();
-            string msg = "안녕하세요.";
-            byte[] datagram = Encoding.UTF8.GetBytes(msg);
+            byte[] datagram = Encoding.UTF8.GetBytes(input);
 
             // (2) 데이터 송신
             udpClient.Send(datagram, datagram.Length, "127.0.0.1", 9999);
