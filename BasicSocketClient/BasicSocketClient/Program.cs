08434a0356d9fdb555db88997edf71c5cf9c08ef
naocya@naver.com <naocya@naver.com>
2023-06-16 22:40:29 +0900

    BasicSocket 콘솔 앱

---
+++ BasicSocketClient/BasicSocketClient/Program.cs
@@ -0,0 +1,66 @@
+﻿// 참고한 깃허브 링크 : https://github.com/jacking75/edu_com2us_CSharpNetworkProgramming/blob/master/BasicSocketClient/BasicSocketClient/Program.cs
+// 참고한 웹 사이트 : http://www.csharpstudy.com/net/article/9-Socket-%ed%81%b4%eb%9d%bc%ec%9d%b4%ec%96%b8%ed%8a%b8
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Baisc TCP Client");
+            StartClient();
+        }
+
+        private static void StartClient()
+        {
+            // (1) 소켓 객체 생성 (TCP 소켓)
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // (2) 서버에 연결
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000);
+                sender.Connect(remoteEP);
+
+                string cmd = string.Empty;
+                byte[] receiverBuff = new byte[8192];
+
+                Console.WriteLine("Connected... Enter Q to exit");
+
+                // Q를 누를 때까지 계속 Echo 실행
+                while ((cmd = Console.ReadLine()) != "Q")
+                {
+                    byte[] buff = Encoding.UTF8.GetBytes(cmd);
+
+                    // (3) 서버에 데이터 전송
+                    int bytesSent = sender.Send(buff);
+
+                    // (4) 서버에서 데이터 수신
+                    int bytesRec = sender.Receive(receiverBuff);
+
+                    Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(receiverBuff, 0, bytesRec));
+                }
+
+                // (5) 소켓 닫기
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected exception : {0}", ex.ToString());
+            }
+        }
+    }
+}
---
+++ BasicSocketServer/BasicSocketServer/Program.cs
@@ -0,0 +1,74 @@
+﻿// 참고한 깃허브 링크 : https://github.com/jacking75/edu_com2us_CSharpNetworkProgramming/blob/master/BasicSocketServer/BasicSocketServer/Program.cs
+// 참고한 웹 사이트 : http://www.csharpstudy.com/net/article/10-Socket-%ec%84%9c%eb%b2%84
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic TCP Server");
+            StartListening();
+        }
+
+        public static void StartListening()
+        {
+            // (1) 소켓 객체 생성 (TCP 소켓)
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+            try
+            {
+                // (2) 포트에 바인드
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000);
+                listener.Bind(localEndPoint);
+
+                // (3) 포트 Listening 시작
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection...");
+
+                // (4) 연결을 받아들여 새 소켓 생성
+                Socket handler = listener.Accept();
+
+                // Data buffer for incoming data.
+                byte[] bytes = new byte[8192];
+
+                string cmd = string.Empty;
+
+                Console.WriteLine("Connected... Enter Q to exit");
+
+                while ((cmd = Console.ReadLine()) != "Q")   // 키 누르면 종료
+                {
+
+                    // (5) 소켓 수신
+                    int bytesRec = handler.Receive(bytes);
+
+                    string data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    Console.WriteLine("Text received : {0}", data);
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    // (6) 소켓 송신
+                    handler.Send(msg);  // echo
+                    handler.Shutdown(SocketShutdown.Both);
+
+                    // (7) 소켓 닫기
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+    }
+}
